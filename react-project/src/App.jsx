b7e7fbe43f9c87c705118d33aaa559ef80084bc1
Heartbeat <heartbeatpkmn@gmail.com>
2022-11-09 00:45:12 +0700

    [ADD] react project setup

---
+++ react-project/src/App.jsx
@@ -0,0 +1,13 @@
+import {createRoot} from "react-dom/client";
+
+const App = () => {
+    return (
+        <div>
+            <h1>React App</h1>
+        </div>
+    );
+};
+
+const container = document.getElementById("root");
+const root = createRoot(container);
+root.render(<App/>);
